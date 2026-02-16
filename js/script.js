4f8b582451c5069b6a1854ba110896124617bd0a
Pranav <pranav.social7@gmail.com>
2026-02-16 19:27:08 +0530

    added google form link in Enroll button

--- js/script.js
+++ js/script.js
@@ -3,13 +3,3 @@ document.querySelectorAll("#year").forEach(el => {
   el.textContent = new Date().getFullYear();
 });
 
-// Enroll button
-function enroll() {
-  alert("For enrollment, please call us.");
-}
-
-// WhatsApp link
-const wp = document.getElementById("whatsappBtn");
-if (wp) {
-  wp.href = "https://wa.me/918210823257";
-}
